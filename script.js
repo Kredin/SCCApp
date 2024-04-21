769e8089409720edac4485c919523c7fb4fa7584
Stefan-webdev <154948992+Stefan-webdev@users.noreply.github.com>
2024-04-21 19:09:52 +0930

    Selectors Done

---
+++ script.js
@@ -0,0 +1,10 @@
+// First things to Add form and container classes. 
+// best to use querySelector instead of getElementbyID, (but no right or wrong answer as both would work)
+const breakfastArea = document.querySelector("#breakfast")
+const lunchArea = document.querySelector("#lunch") 
+const dinnerArea = document.querySelector("#dinner")
+const snackArea = document.querySelector("#snack")
+const excerciseArea = document.querySelector("#excercise")
+
+const calorieData = document.querySelector("#calorieData")
+// Don't need to gather 
