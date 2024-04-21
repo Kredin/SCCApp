621b0266bd33e2a40d4105a487ef0f0ed9b1f0f9
Blade <bladeprojectz@gmail.com>
2024-04-21 17:55:45 +0800

    event listener added and functions started

--- script.js
+++ script.js
@@ -1,10 +1,45 @@
-// First things to Add form and container classes. 
+// First things to Add form and container classes.
 // best to use querySelector instead of getElementbyID, (but no right or wrong answer as both would work)
-const breakfastArea = document.querySelector("#breakfast")
-const lunchArea = document.querySelector("#lunch") 
-const dinnerArea = document.querySelector("#dinner")
-const snackArea = document.querySelector("#snack")
-const excerciseArea = document.querySelector("#excercise")
+const breakfastArea = document.querySelector("#breakfast");
+const lunchArea = document.querySelector("#lunch");
+const dinnerArea = document.querySelector("#dinner");
+const snackArea = document.querySelector("#snack");
+const excerciseArea = document.querySelector("#excercise");
 
-const calorieData = document.querySelector("#calorieData")
-// Don't need to gather 
+const calorieData = document.querySelector("#calorieData");
+
+let totalCalories = 0;
+
+calorieData.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const category = document.querySelector("#category");
+  const description = document.querySelector("#description");
+  const calories = document.querySelector("#calories");
+  console.log(category.value, description.value, calories.value);
+
+  switch (category) {
+    case "breakfast":
+      addBreakfast(category, description, calories);
+      break;
+    case "lunch":
+      addLunch(category, description, calories);
+      break;
+    case "dinner":
+      addDinner(category, description, calories);
+      break;
+    case "snack":
+      addSnack(category, description, calories);
+    case "excercise":
+      addExcercise(category, description, calories);
+  }
+});
+
+function addBreakfast(category, description, calories) {}
+
+function addLunch(category, description, calories) {}
+
+function addDinner(category, description, calories) {}
+
+function addSnack(category, description, calories) {}
+
+function addExcercise(category, description, calories) {}
